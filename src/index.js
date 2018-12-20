2a7826bf5aa9dc3c7b59b633ac69d4b0d4b0b9d9
Marta Niemiec <niemiec.marta@hotmail.com>
2018-12-20 10:59:51 +0100

    created actual date in header

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './scss/main.scss';
 
+import { elements, months } from './js/views/base.js';
+
+// DATE ON HEADER
+let date = new Date();
+let day = date.getDate();
+let month = date.getMonth();
+let year = date.getFullYear();
+
+elements.headerDate.innerHTML = `${day} ${months[month]} ${year}`;
---
+++ src/js/views/base.js
@@ -0,0 +1,8 @@
+export const elements = {
+  headerDate: document.querySelector('.header__date')
+}
+
+export const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+
