6f84feaa83cfc668397553c3dcf7ccf33c32e8af
Marta Niemiec <niemiec.marta@hotmail.com>
2019-01-30 11:12:04 +0100

    moved genereteWeeks() to the month module and added some comments

--- src/js/models/month.js
+++ src/js/models/month.js
@@ -1,7 +1,35 @@
 import { elements } from '../views/base';
-import { months } from './week';
+import * as Week from './Week';
 
 
+const weeks = [];
+
+// display mont name in the month section
 export const displayMonth = (monthNumber) => {
-  elements.month.innerHTML = months[monthNumber.getMonth()];
+  elements.month.innerHTML = Week.months[monthNumber.getMonth()];
 }
+
+// generate all weeks numbers, theirs first and tast day from the current year 
+const genereteWeeks = () => {
+  const firstDayYear = new Date(Week.lastChosedDay.getFullYear(), 0, 1);
+  // console.log(firstDayYear);
+  for (let i = 0; i<=51; i++) {
+    // console.log("todayDate", lastChosedDay);
+    let firstDay = Week.startOfWeek(new Date(firstDayYear.getTime() + 7 * i * 24 * 60 * 60 * 1000), 0);
+    // console.log("todayDate", firstDay.getMonth());
+    firstDay++;
+    const newDay = new Date(firstDay);
+    const first = newDay.toString().split(" ", 4);
+    const last = new Date((newDay.getTime() + 6 * 24 * 60 * 60 * 1000)).toString().split(" ", 4);
+    // weeks.push(newDay.getDate());
+    weeks.push({
+      weekNr: i+1,
+      firstDay: `${first[2]} ${first[1]}`, 
+      lastDay: `${last[2]} ${last[1]}`
+    })
+  }
+  return weeks;
+}
+
+genereteWeeks();
+console.log(weeks);
--- src/js/models/week.js
+++ src/js/models/week.js
@@ -1,5 +1,5 @@
 import { elements } from '../views/base';
-import * as Month from './month'
+import * as Month from './Month'
 
 // DATE ON HEADER
 export let todayDate = new Date();
@@ -96,29 +96,7 @@ const changeWeek = (date, count) => {
 
 
 
-const weeks = [];
-const genereteWeeks = () => {
-  const firstDayYear = new Date(lastChosedDay.getFullYear(), 0, 1);
-  // console.log(firstDayYear);
-  for (let i = 0; i<=51; i++) {
-    // console.log("todayDate", lastChosedDay);
-    let firstDay = startOfWeek(new Date(firstDayYear.getTime() + 7 * i * 24 * 60 * 60 * 1000), 0);
-    // console.log("todayDate", firstDay.getMonth());
-    firstDay++;
-    const newDay = new Date(firstDay);
-    const first = newDay.toString().split(" ", 4);
-    const last = new Date((newDay.getTime() + 6 * 24 * 60 * 60 * 1000)).toString().split(" ", 4);
-    // weeks.push(newDay.getDate());
-    weeks.push({
-      weekNr: i+1,
-      firstDay: `${first[2]} ${first[1]}`, 
-      lastDay: `${last[2]} ${last[1]}`
-    })
-  }
-  return weeks;
-}
-genereteWeeks();
-console.log(weeks);
+
 
 
 
