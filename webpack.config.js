c12a668b557b16efa03df1bb19ce5a24bae52e8c
Marta Niemiec <niemiec.marta@hotmail.com>
2019-04-17 16:05:01 +0200

    installed case-sensitive-paths-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -53,6 +54,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
-    })
+    }),
+    new CaseSensitivePathsPlugin({debug: true})
   ]
 };
